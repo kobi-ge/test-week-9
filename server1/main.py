bc16d85fb9664d4567a098d9a77c999d2c0554c3
kobi <kodigershuni@gmail.com>
2025-12-17 13:06:33 +0200

    server 1 almost ready

--- server1/main.py
+++ server1/main.py
@@ -1,19 +1,23 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from typing import Optional, Dict
 import json
 from pathlib import Path
 
 
 app = FastAPI(title="Items API", version="1.0.0")
 
-DB_PATH = Path("server1/db/shopping_list.json")
+class Item(BaseModel):
+    name: str
+    quantity: str
 
 def create_shopping_list():
-    with open("server1/db/shopping_list.json", "w") as f:
+    with open("db/shopping_list.json", "w") as f:
         f.write("[]")
+create_shopping_list()
 
-def load_database() -> Dict:
+DB_PATH = Path("db/shopping_list.json")
+
+def load_database():
     try:
         with open(DB_PATH, "r") as f:
             return json.load(f)
@@ -27,6 +31,26 @@ def get_items():
     return data
 
 
+def update_db(updated_data):
+    with open("db/shopping_list.json", "w") as f:
+        json.dump(updated_data, f, indent=2)
+
+def create_dict(item: Item):
+    data = {"name": item.name, "quantity": item.quantity}
+    return data
+
+@app.post("/item")
+def add_item(item: Item):
+    data = load_database()
+    item = create_dict(item)
+    new_id = str(len(data) + 1)
+    item[id] = new_id
+    data.append(item)
+    update_db(data)
+    return data
+
+
+    
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app)
